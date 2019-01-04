a2cafb48af3a924ca67bf8c1db0c2a88a7adf61c
Manjeet Singh <designermanjeets@gmail.com>
2019-01-04 18:57:48 +0530

    NavbarComponent works

--- msv7-workspace/libs/msv4knw-nav/src/lib/msv4knw-nav.module.ts
+++ msv7-workspace/libs/msv4knw-nav/src/lib/msv4knw-nav.module.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule],
+  exports: [NavbarComponent],
   declarations: [NavbarComponent]
 })
 export class Msv4knwNavModule {}
