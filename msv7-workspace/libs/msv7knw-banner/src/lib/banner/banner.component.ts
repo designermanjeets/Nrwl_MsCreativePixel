2d345eb19ddc4440b42e4f2e3837fc6dd17082e8
Manjeet Singh <designermanjeets@gmail.com>
2019-01-07 15:13:05 +0530

    banner

--- msv7-workspace/libs/msv7knw-banner/src/lib/banner/banner.component.ts
+++ msv7-workspace/libs/msv7knw-banner/src/lib/banner/banner.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { map, debounceTime, distinctUntilChanged, delayWhen, retryWhen, switchMap, startWith } from 'rxjs/Operators';
+import { fromEvent, timer, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'msv7banner-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('searchInput') searchInput : ElementRef
  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    fromEvent<KeyboardEvent>(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        // map(event => event.target.value), // Not Good
+        debounceTime(400), // Wait for the period of Silence for 400 ml || Delay
+        startWith(''),
+        distinctUntilChanged(),
+        switchMap((search: KeyboardEvent) => {
+          const inputVal = (<HTMLInputElement>this.searchInput.nativeElement).value;
+          return of(this.searchTerm(inputVal))
+        }),
+        retryWhen(errors => errors.pipe(delayWhen(() => timer(2000))) ) // Retry after 2 sec and subscribe again HTTP stream
+      )
+      .subscribe();
+    
+  }
+
+  searchTerm(term: string) {
+    console.log(term);
   }
 
   onSubmit(form){
